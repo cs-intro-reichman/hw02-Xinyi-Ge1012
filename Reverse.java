cbd78f84d45af56e6ceb8941e6f81988b0b171e9
Xinyi-Ge1012 <154273174+Xinyi-Ge1012@users.noreply.github.com>
2023-12-29 19:44:01 +0200

    Update Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -6,10 +6,12 @@ public class Reverse {
 	public static void main (String[] args){
 	String s = args[0];
 		int i;
-		for (i=0; i <= (s.length()-1); i++ ){
+		for (i=0;i < (s.length()-1) ; i++ ){
            System.out.print(s.charAt(s.length()-1-i));
         }
-		System.out.println(" ");
+	   if (i = (s.length()-1)) {
+		System.out.println(s.charAt(0));
+	   }
 		if(s.length() % 2 ==0){
 			System.out.println("The middle character is " + s.charAt(s.length()/2-1));
 		} else {
