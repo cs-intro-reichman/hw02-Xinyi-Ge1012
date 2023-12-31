31b7fa4e2c2d5c7f4cc52a4533720c06dfaa52e2
Xinyi-Ge1012 <154273174+Xinyi-Ge1012@users.noreply.github.com>
2023-12-31 22:38:27 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,7 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
+	 int n = Integer.parseInt(args[0]);
 	  for(int i=0; i<n; i++){ 
 		if(i % 2 == 1){ 
 			System.out.print(" ");
